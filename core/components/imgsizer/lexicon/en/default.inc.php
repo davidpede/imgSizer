2bc5cadf8eb45cbe9238be69505a899fb367af25
David Pede <david@tasian.media>
2016-10-28 13:21:39 +0100

    Add EN lexicon file

---
+++ core/components/imgsizer/lexicon/en/default.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Default Lexicon Entries for imgSizer
+ *
+ * @package imgsizer
+ * @subpackage lexicon
+ */
+
+$_lang['imgsizer'] = 'imgSizer';
+
+$_lang['setting_imgsizer.cache_path'] = 'imgSizer Cache Path';
+$_lang['setting_imgsizer.cache_path_desc'] = 'Optional Cache location when using imgSizer. Relative to MODX_BASE_DIR. Setting a value for \'imgsizer.cache_url\' is REQUIRED.';
+$_lang['setting_imgsizer.cache_url'] = 'imgSizer Cache URL';
+$_lang['setting_imgsizer.cache_url_desc'] = 'An absolute URL to the generated images. Can be useful when using contexts and subdomains. Setting a value for \'imgsizer.cache_path\' is REQUIRED.';
